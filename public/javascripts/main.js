e42ca0fdb54a59a4e480a5b89de71dc72b7f0fb5
Kaique da Silva <kaique.developer@gmail.com>
2012-09-22 06:21:12 +0000

    inup

---
+++ public/javascripts/main.js
@@ -0,0 +1,10 @@
+require([
+    '/javascripts/lib/underscore.js',
+    '/javascripts/lib/jquery.js',
+    '/javascripts/lib/json2.js',
+    '/javascripts/lib/backbone.js'
+], function () {
+    $(function () {
+        
+    });
+});
