e6ef7ecd830e1c00a06935289c51b5627494d069
Kaique da Silva <kaique.developer@gmail.com>
2012-09-22 09:52:01 +0000

    code refactor

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,7 +8,14 @@ require([
         var AbacoModel = Backbone.Model.extend({
             defaults: {
                 label: 'Start?',
-                answer: null
+                answer: null,
+                limit: 11,
+                signals: [
+                    '+',
+                    '-',
+                    '÷',
+                    'x'
+                ]
             },
 
             start: function () {
@@ -20,59 +27,87 @@ require([
                 });    
             },
 
+            randomSignal: function () {
+                var id = Math.round( Math.random() * this.get('signals').length );    
+
+                return this.get('signals')[ id ];
+            },
+
+            randomNumber: function () {
+                return Math.round( Math.random() * this.get('limit') );
+            },
+
+            sum: function ( a, b ) {
+                return a + b;    
+            },
+
+            sub: function ( a, b ) {
+                return a - b;    
+            },
+
+            div: function ( a, b ) {
+                return a / b;    
+            },
+
+            mult: function ( a, b ) {
+                return a * b;    
+            },
+
+            expression: function ( a, operator, b ) {
+                var output = 0;
+
+                switch ( operator ) {
+                    case '+':    
+                        output = this.sum( Number( a ), Number( b ) ); 
+                    case '-':    
+                        output = this.sub( Number( a ), Number( b ) ); 
+                    case '÷':    
+                        output = this.div( Number( a ), Number( b ) ); 
+                    case 'x':    
+                        output = this.mult( Number( a ), Number( b ) ); 
+                }
+
+                return output;
+            },
+
+            label: function ( a, operator, b ) {
+                return String( a ) +
+                       String( operator ) +
+                       String( b );
+            },
+
+            answer: function ( a, operator, b ) {
+                return this.expression(
+                    a,
+                    operator,
+                    b
+                );    
+            },
+
             newChallenge: function () {
-                var signals = [
-                    '+',
-                    '-',
-                    '÷',
-                    'x'
-                ], 
-                limitValue = 100,
-                limitExpression = 2,
-                label = '',
-                answer = undefined;
+                var output = {
+                        label: '',
+                        answer: 0
+                    },
+                    expression = [
+                        this.randomNumber(),
+                        this.randomSignal(),
+                        this.randomNumber()
+                    ];
 
-                // Random signal
-                var signalID = Math.round( Math.random() * signals.length );
+                output['label'] = this.label(
+                    expression[0],
+                    expression[1],
+                    expression[2]
+                ); 
 
-                label = ( ( Math.round( Math.random() * limitValue ) ) + 
-                        ( signals[ signalID ] ) +
-                        ( Math.round( Math.random() * limitValue ) ) ).toString();
+                output['answer'] = this.answer(
+                    expression[0],
+                    expression[1],
+                    expression[2]
+                );
 
-                answer = function ( label ) {
-                    var label = label,
-                        output = 0,
-                        counter = 0;
-
-                    if ( label.indexOf( signals[0] ) > -1 ) {
-                        label = label.split( signals[0] );
-                        counter = label.length;
-
-                        output = Number( label[0] ) + Number( label[1] );
-                    } else if ( label.indexOf( signals[1] ) > -1 ) {
-                        label = label.split( signals[1] );
-                        counter = label.length;
-
-                        output = Number( label[0] ) - Number( label[1] );
-                    } else if ( label.indexOf( signals[2] ) > -1 ) {
-                        label = label.split( signals[2] );
-                        counter = label.length;
-
-                        output = Number( label[0] ) / Number( label[1] );
-                    } else if ( label.indexOf( signals[3] ) > -1 ) {
-                        label = label.split( signals[3] );
-                        counter = label.length;
-
-                        output = Number( label[0] ) * Number( label[1] );
-                    } 
-
-                    return output;
-                };
-
-                return {
-                    label: label,
-                    answer: answer( label )
-                };
+                return output;
             }
         });
 
@@ -122,7 +157,7 @@ require([
                     userAnswer = $('#answer-input'); 
 
                 if ( userAnswer.val() ) {
-                    if ( app.abacoModel.get('answer').toFixed(2) === userAnswer.val() ) {
+                    if ( app.abacoModel.get('answer').toString() === userAnswer.val() ) {
                         alert( 'Correct!' );
                     } else {
                         alert( 'Incorrect!' );    
